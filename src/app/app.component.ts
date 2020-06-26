cc086ec2efd9ca868a6b29b44673b60eab06a54a
sknasarvali <sknasarvali007@gmail.com>
2020-06-26 15:36:58 +0530

    task done and uploaded

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'taskCom';
+  registerForm:FormGroup;
+  submitted=false;
+
+  constructor(private formbuilder:FormBuilder){ }
+
+  ngOnInit(){
+    this.registerForm=this.formbuilder.group({
+            firstname: ['', Validators.required],
+            lastname: ['', Validators.required],
+            username: ['', Validators.required],
+            password: ['', [Validators.required, Validators.minLength(10)]],
+            date: ['', Validators.required],
+            country: ['', Validators.required]
+
+    });
+
+  }
+
+  get f() { return this.registerForm.controls; }
+
+  onSubmit() {
+    this.submitted = true;
+
+    
+    if (this.registerForm.invalid) {
+        return;
+    }
+
+    alert('REGISTRATION SUCCESS');
+}
 }
